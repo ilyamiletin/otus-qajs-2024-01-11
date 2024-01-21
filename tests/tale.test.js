cdd86373cec370f67f8c48723b2ca6092e237a3c
Ilya Miletin <imiletin@yandex.ru>
2024-01-21 21:25:50 +0300

    написал тесты в папке tale.test.js

---
+++ tests/tale.test.js
@@ -0,0 +1,43 @@
+import { kolobok, newYear } from "../src/tale";
+
+// тест писал Рома на вебинаре
+describe ('kolobok', () => {
+    it ('should return kolobok дедушка', () => {
+        const result = kolobok('дедушка')
+        expect(result).toBe('Я от дедушки ушёл')
+    })
+})
+
+// тест писал Дамир на вебинаре
+describe ('function kolobok', () => {
+    it ('can see strange', () => {
+        try {
+            kolobok('Дедушку мороза')
+            console.log('мы этого не увидим')
+        } catch (error) {
+            expect(error.message).toBe('Я встретил кого-то неизвестного')
+        }
+    })
+})
+
+// тест писал Я по образцу из презентации
+describe ('function kolobok 2', () => {
+    it ('can see strange 2', () => {
+        expect(() => {
+            kolobok('Дедушку мороза')
+        }).toThrow('Я встретил кого-то неизвестного')
+    })
+})
+
+// тест который писал Я, но по паттерну AAA
+describe ('function kolobok 3', () => {
+    it ('can see strange 3', () => {
+        const name = 'Дедушку мороза'
+
+        const prepare = () => {
+            kolobok(name)
+        }
+
+        expect(prepare).toThrow('Я встретил кого-то неизвестного')
+    })
+})

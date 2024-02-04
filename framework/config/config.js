a80bd3de445d2f3dad1fba858675e0d142ac2bb7
Ilya Miletin <imiletin@yandex.ru>
2024-02-04 21:37:57 +0300

    add API tests bookstore/homework

--- framework/config/config.js
+++ framework/config/config.js
@@ -2,13 +2,11 @@ import 'dotenv/config'
 
 // process.env.TEST_BASE_API_URL = undefined
 
-const apiConfig = {
+const config = {
     baseURL: process.env.TEST_BASE_API_URL ?? 'http.....',
     userId: process.env.TEST_USER_ID,
     username: process.env.TEST_USERNAME,
     password: process.env.TEST_PASSWORD,
 }
 
-console.log(apiConfig)
-
-
+export default Object.freeze(config)

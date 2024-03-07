90acbe9ba402755ec4205249a76cdc534f87d464
Ilya Miletin <imiletin@yandex.ru>
2024-03-08 01:34:58 +0300

    add jest html

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ const config = {
     testEnvironment: 'allure-jest/node',
     setupFiles: ['dotenv/config'],
     verbose: true,
+    reporters: [
+        'default',
+        'jest-html-reporters'
+      ]
 }
 
 module.exports = config

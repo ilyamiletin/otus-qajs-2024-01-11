46a2cc09003e79bd359ea1748dc6f3f9fcf4cd88
Ilya Miletin <imiletin@yandex.ru>
2024-03-08 02:52:00 +0300

    changed jest_config

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,7 @@ const config = {
     testEnvironment: 'allure-jest/node',
     setupFiles: ['dotenv/config'],
     verbose: true,
-    reporters: [
-        'default',
-        'jest-html-reporters'
-      ]
+    reporters: ['default', 'jest-html-reporters'],
 }
 
 module.exports = config
